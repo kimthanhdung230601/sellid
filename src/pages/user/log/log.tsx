605fbd7e66b6efcad84baba734dab309bef1456a
dung.kimthanh <dung.kimthanh@amela.vn>
2024-02-28 22:48:15 +0700

    create user

---
+++ src/pages/user/log/log.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import styles from "./styles.module.scss";
+import Register from "./register";
+import LogInComponent from "./logIn";
+interface LogInProps {}
+
+const LogIn = () => {
+    const [selected,setSelected] = useState("login");
+    const handleClickSelect = (value:any)=>{
+        setSelected(value);
+    }
+  return (
+    <>
+      <div className={styles.wrap}>
+        <div className={styles.logWrap}>
+            <div className={styles.titleGroup}>
+                <h3 className={`${styles.title} ${selected ==="login"? styles.selected :""}`  } onClick={()=>handleClickSelect("login")}>Đăng nhập</h3>
+                <h3 className={`${styles.title} ${selected ==="register"? styles.selected :""}`} onClick={()=>handleClickSelect("register")}>Đăng ký</h3>
+            </div>
+            <div className={styles.formGroup}>
+                {selected ==="login" ? <LogInComponent/> : <Register/>}
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LogIn;
--- src/wrapper/index.tsx
+++ src/wrapper/index.tsx
@@ -3,6 +3,7 @@ import Layout from "../pages/admin/layout/Layout";
 import Home from "../pages/admin/home/home";
 import Product from "../pages/admin/addNew/product";
 import Profile from "../pages/admin/profile/profile";
+import LogIn from "../pages/user/log/log";
 
 const Wrapper = () => {
   return (
@@ -12,6 +13,9 @@ const Wrapper = () => {
           <Route index path="/admin" element={<Home/>} />
           <Route path="/admin/product"  element={<Product/>}/>
           <Route path="/admin/profile" element={<Profile/>}/>
+        </Route>
+        <Route path="/" >
+          <Route path="/logIn" element={<LogIn/>} />
         </Route>
       </Routes>
     </>
