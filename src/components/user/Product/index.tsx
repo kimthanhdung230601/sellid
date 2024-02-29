1a6ee413b273bda885456d87ff242164246e752e
Duyen <ngoc.ly135@gmail.com>
2024-02-29 17:20:48 +0700

    product update

--- src/components/user/Product/index.tsx
+++ src/components/user/Product/index.tsx
@@ -33,8 +33,8 @@ export default function Product() {
   };
   return (
     <>
-    <Col className='gutter-row' xxl={4} xl={6} lg={6} md={8} sm={12} xs={12} style={{marginBottom: "30px", marginTop:"30px"}}>
-        <div className={style.wrap}>
+    <Col xxl={5} xl={6} lg={6} md={8} sm={12} xs={12} className={`${style.wrap} gutter-row`}>
+        <div >
             <div className={style.imageWrap}>
            <Image src={require("../../../assets/image/idcard.png")} className={style.img}/> 
         </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,34 +8,59 @@ export default function Home() {
     <div className={style.wrap}>
       <Header />
       <div className={style.container}>
-        <Row gutter={50} justify="center" className={style.row}>
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-            <Product />
-      </Row>
+        <Row gutter={30}>
+          <Col xxl={4} xl={4} lg={24} md={24} sm={24} xs={24}>
+            <div className={style.filter}>Lọc chuyên mục</div>
+            <div className={style.category}>
+              <span className={style.categoryName}>Tất cả</span>
+                 
+            </div>
+            <div className={style.category}>
+              <span className={style.categoryName}>Việt Nam</span>
+            </div>
+            <div className={style.category}>
+              <span className={style.categoryName}>Indo</span>
+            </div>
+            <div className={style.category}>
+              <span className={style.categoryName}>Thái Lan</span>
+            </div>
+            <div className={style.category}>
+              <span className={style.categoryName}>Việt Nam</span>
+            </div>
+          </Col>
+          <Col xxl={20} xl={20} lg={24} md={24} sm={24} xs={24}>
+            <Row gutter={50} justify="center" className={style.row}>
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+          </Row>
+      </Col>
+
+        </Row>
+        
       </div>
       
     </div>
