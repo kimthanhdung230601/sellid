8fc873ce43e0c4a9be147e9f610eecba7bb0cc0b
Kim Thanh Dung <105005391+kimthanhdung230601@users.noreply.github.com>
2024-03-18 22:41:14 +0700

    update pagination

--- src/pages/admin/home/home.tsx
+++ src/pages/admin/home/home.tsx
@@ -184,12 +184,12 @@ const HomeAdmin = () => {
             {" "}
             <>
               {" "}
-              <Table
-                columns={columns}
-                dataSource={product?.data}
-                style={{ overflowX: "auto" }}
-                pagination={false}
-                 
+              <Pagination
+                defaultCurrent={1}
+                onChange={onChange}
+                total={product?.total_products}
+                pageSize={12}
+                style={{ margin: "1vh 0", float: "right" }}
               />
               <Pagination
                 defaultCurrent={1}
